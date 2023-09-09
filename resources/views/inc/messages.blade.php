b5842991c1c9db75a76612cb70ef9babc1e78b85
Sabelo-Mdashe <selbysabelomdashe@gmail.com>
2023-09-09 23:34:03 +0200

    clean-up

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,6 +1,6 @@
 @if (count($errors) > 0)
     @foreach ($errors->all() as $error)
-        <div class="alert alert-danger">
+        <div @class(['alert', 'alert-danger'])>
             {{ $error }}
         </div>
     @endforeach
@@ -8,14 +8,14 @@
 @endif
 
 @if (session('success'))
-    <div class="alert alert-success">
+    <div @class(['alert', 'alert-success'])>
         {{ session('success') }}
     </div>
     
 @endif
 
 @if (session('error'))
-    <div class="alert alert-danger">
+    <div @class(['alert', 'alert-danger'])>
         {{ session('error') }}
     </div>
     
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
     @section('content')
-        <div class="mt-5 text-center bg-body-secondary p-5">
+        <div @class(['mt-5', 'text-center', 'bg-body-secondary', 'p-5'])>
             <h1>{{ $title }}</h1>
             <p>Please Login or Register if you don't have an account already</p>
-            <div class="d-flex gap-3 justify-content-center">
+            <div @class(['d-flex', 'gap-3', 'justify-content-center'])>
                 <a href="">
-                    <button type="button" class="btn btn-primary">Login</button>
+                    <button type="button" @class(['btn', 'btn-primary'])>Login</button>
                 </a>
                 <a href="">
-                    <button type="button" class="btn btn-success">Register</button>
+                    <button type="button" @class(['btn', 'btn-success'])>Register</button>
                 </a>
             </div>
         </div>  
