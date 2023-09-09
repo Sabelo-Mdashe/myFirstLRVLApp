85a447c7b4912cf285f3b445e9c2136bc20b2b64
Sabelo-Mdashe <selbysabelomdashe@gmail.com>
2023-09-09 23:58:26 +0200

    last push today

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,10 +9,12 @@
         <hr>
         <small>Written on {{ $post->created_at }}</small>
         <br>
-        <a href="/posts/{{$post->id}}/edit" @class(['btn', 'btn-default', 'btn-light', 'mt-5'])>Edit</a>
-        
-        {{ html()->form('POST')->open(['action' => 'PostsController@destroy', $post->id]) }}
-            @method('DELETE')
-            {{ html()->submit('Delete')->class('btn btn-danger mt-5') }}
-        {{ html()->form()->close() }}
+        <div @class(['d-flex', 'gap-3'])>
+            <a href="/posts/{{$post->id}}/edit" @class(['btn', 'btn-default', 'btn-light', 'mt-5'])>Edit</a>
+            
+            {{ html()->form('POST')->open(['action' => 'PostsController@destroy', $post->id]) }}
+                @method('DELETE')
+                {{ html()->submit('Delete')->class('btn btn-danger mt-5') }}
+            {{ html()->form()->close() }}
+        </div>
     @endsection
